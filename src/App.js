11dbaaa15c1c13970b91446684dab16da0b9a06e
ambahjones <67397295+ambahjones@users.noreply.github.com>
2020-08-25 21:54:44 -0400

    removed unnecessary items and readied search for api call

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import "./App.css";
 import Search from "./Search";
-import Current from "./Current";
 import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="App">
       <Search />
-      <Current />
       <Footer />
     </div>
   );
--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import axios from "axios";
+import Current from "./Current";
 
 export default function Search() {
+  let [city, setCity] = useState("");
+  let [weather, setWeather] = useState("");
+
+  function showWeather(response) {
+    setWeather(response.data);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "093e071e943ff87f9fc6c8107074ad0a";
+    let unit = "imperial";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+    axios.get(url).then(showWeather);
+  }
+
+  function enteredCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+
   return (
     <div>
       <form>
@@ -11,12 +33,14 @@ export default function Search() {
           className="searchBar"
           id="city-to-search"
           autofocus="on"
+          onSubmit={handleSubmit}
         />
         <input
           type="submit"
           value="Search"
           className="searchButton"
           id="search"
+          onChange={enteredCity}
         />
         <input
           type="submit"
@@ -25,6 +49,7 @@ export default function Search() {
           id="current"
         />
       </form>
+      <Current weather={weather} />
     </div>
   );
 }
